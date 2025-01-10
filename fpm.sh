cf2eb6371b6f806b98691a8c310f4f18b1b7a592
macbook <metides30@gmail.com>
2025-01-10 12:42:25 +1100

    Refactor CPU and memory usage logging in fpm.sh
    - Updated the script to echo CPU and memory usage only when thresholds are exceeded, improving log clarity.
    - Ensured that the logging format remains consistent with timestamps for better monitoring.
    
    These changes enhance the script's maintainability and provide clearer insights into system resource usage.

--- fpm.sh
+++ fpm.sh
@@ -5,16 +5,16 @@ cores=$(nproc)
 load=$(awk '{print $1}' </proc/loadavg)
 cpuUsage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
 now=$(date)
-echo "[$now]: $cpuUsage% is using by CPU."
 
 if [ $cpuUsage -ge 90 ]; then
+  echo "[$now]: $cpuUsage% is using by CPU."
   sudo systemctl restart php-fpm
 fi
 
 freeMemory=$(free -m | grep Mem | awk '{print int($4/$2*100)}')
 now=$(date)
-echo "[$now]: $freeMemory% free Memory"
 
 if [ $freeMemory -lt 20 ]; then
+  echo "[$now]: $freeMemory% free Memory"
   sudo systemctl restart php-fpm
 fi
