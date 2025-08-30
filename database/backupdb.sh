4beb3cbe37979bdc812c42b57a5271d6a9b2a780
macbook <metides30@gmail.com>
2025-08-30 15:25:04 +1000

    Add database backup script and README documentation

--- database/backupdb.sh
+++ database/backupdb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Configurations
-DB_USER="ittdb"          # Change to your MariaDB username
-DB_PASS="Ifeq1xBCdIR481"      # Change to your MariaDB password
+DB_USER="itt-admin"          # Change to your MariaDB username
+DB_PASS="GCdGb!fNmk!!3dH"      # Change to your MariaDB password
+DB_HOST="152.69.175.15"
 BACKUP_DIR="$HOME/backups"        # Change if needed
 RETENTION_DAYS=3             # Number of days to keep backups
 
@@ -10,7 +11,7 @@ RETENTION_DAYS=3             # Number of days to keep backups
 mkdir -p "$BACKUP_DIR"
 
 # Get a list of all databases (excluding system databases)
-DATABASES=$(mysql -u $DB_USER -p$DB_PASS -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)")
+DATABASES=$(mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)")
 
 # Loop through each database and create a backup
 for DB in $DATABASES; do
@@ -18,7 +19,7 @@ for DB in $DATABASES; do
     BACKUP_FILE="$BACKUP_DIR/${DB}_backup_$TIMESTAMP.sql.gz"
 
     echo "[+] Creating backup for database: $DB"
-    mysqldump -u $DB_USER -p$DB_PASS $DB | gzip > "$BACKUP_FILE"
+    mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS --single-transaction --set-gtid-purged=OFF $DB | gzip > "$BACKUP_FILE"
 
     echo "[✔] Backup completed: $BACKUP_FILE"
 done
