f9719f8670b01da267314be66c020296dabfc5e0
macbook <metides30@gmail.com>
2025-09-14 16:32:11 +1000

    Remove pure-log.py script that filters out lines containing 'WP_MEMORY_LIMIT' from error log

--- websites/salamander/pure-log.py
+++
@@ -1,9 +0,0 @@
-input_file = "/Users/meti/Projects/wp-ansible/logs/salamander/error.log"
-output_file = "/Users/meti/Projects/wp-ansible/logs/salamander/error_cleaned.log"
-
-with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-    for line in infile:
-        if "WP_MEMORY_LIMIT" not in line:
-            outfile.write(line)
-
-print(f"Lines containing 'WP_MEMORY_LIMIT' have been removed. Cleaned file saved as {output_file}.")
